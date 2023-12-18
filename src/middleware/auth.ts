ea307bffb4a0010542f2ea7bda6de7d0656c287a
couscousbawl <hamdi.iadh@gmail.com>
2023-12-18 13:31:25 +0300

    adding the auth middleweare

--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -2,20 +2,19 @@ import { NextFunction, Request, Response } from 'express';
 import { StatusCodes } from 'http-status-codes';
 import jwt from '../utils/jwt';
 
-export default (req: Request, res: Response, next: NextFunction) => {
-  const token = req.headers.authorization;
-
-  if (!token)
-    return next({
-      status: StatusCodes.UNAUTHORIZED,
-      message: 'No token provided',
-    });
+const authorize = (req: Request, res: Response, next: NextFunction) => {
 
   try {
-    const data = jwt.verify(token);
-    res.locals.payload = data;
-    return next();
-  } catch {
-    return next({ status: StatusCodes.UNAUTHORIZED, message: 'Unauthorized' });
+    const token = req.headers.authorization!.split(' ')[1];
+    const decode = jwt.verify(token);
+    req.body.user = decode;
+    next();
+  } catch (error) {
+    res.status(401).json({
+      message: "Unauthorized!",
+      data: req.body
+  });
   }
 };
+
+export default authorize;
--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import UserController from "../controllers/user.controller";
+import authorize from "../middleware/auth";
 
 class UsersRoutes {
   router = Router();
@@ -13,7 +14,7 @@ class UsersRoutes {
     this.router.post('/create', this.user.create); // create a new user
     this.router.get('/:id', this.user.getUserById); // get a user by ID
     this.router.delete('/:id', this.user.deleteUser); // delete a specific user
-    this.router.put('/:id', this.user.update); // update a specific user
+    this.router.put('/:id', authorize, this.user.update); // update a specific user
     this.router.get('', this.user.getAllUsers); // get all users
   }
 }
