2b2a3f30f6376a451acee4570ae047ad8697f2b0
couscousbawl <hamdi.iadh@gmail.com>
2023-12-23 09:53:55 +0300

    removing email token authtentication

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -10,83 +10,85 @@ import jwt from "../utils/jwt";
 const API_TOKEN_EXPIRATION_MINUTES = 1000
 
 interface LoginInput {
-  email: string,
-  password: string
+    email: string,
+    password: string
 }
 
 export default class AuthController {
-    async login(req: Request, res: Response, next: NextFunction){
+    async login(req: Request, res: Response, next: NextFunction) {
         const { email, password } = req.body as LoginInput;
         const tokenExpiration = add(new Date(), { minutes: API_TOKEN_EXPIRATION_MINUTES });
 
         if (!email || !password) {
-            return next({
-              status: StatusCodes.BAD_REQUEST,
-              message: 'Some required fields are missing',
+            res.status(401).json({
+                message: "Some required fields are missing",
+                data: req.body
             });
-          }
+            return next();
+        }
 
-          const user = await prisma.user.findUnique({ where: { email } });
+        const user = await prisma.user.findUnique({ where: { email } });
 
-          if (!user) {
+        if (!user) {
             res.status(404).json({
                 message: "User not Found",
                 email: email
             });
-            return next({
-              status: StatusCodes.NOT_FOUND,
-              message: 'User not found',
-            });
-          }
+            return next();
+        }
 
-          const isValidPassword = await bcrypt.compare(password, user.password);
+        const isValidPassword = await bcrypt.compare(password, user.password);
 
-          if (!isValidPassword) {
+        if (!isValidPassword) {
             res.status(401).json({
                 message: "Invalid Password",
                 data: isValidPassword
             });
-            return next({
-              status: StatusCodes.UNAUTHORIZED,
-              message: 'Invalid password',
-            });
-          }
-
-          const token = jwt.sign({ id: user.id, email: user.email });
-
-        try {
-            const createdToken = await prisma.token.create({
-                data: {
-                    apiToken: token,
-                    type: TokenType.API,
-                    expiration: tokenExpiration,
-                    user: {
-                        connect: {
-                            email: email
-                        }
-                    }
-                },
-                select: {
-                    apiToken: true,
-                    user: {
-                        select: {
-                            email: true
-                        }
-                    }
-                }
-            });
-            // //send the email token
-            //await sendEmailToken(email, emailToken);
-            res.status(201).json({
-                message: "login OK",
-                data: createdToken
-            });
-        } catch (error) {
-            res.status(500).json({
-                message: "Internal Server Error!",
-                error: error
-            });
+            return next();
         }
+
+        const token = jwt.sign({ id: user.id, email: user.email });
+
+        res.status(201).json({
+            message: "login OK",
+            user: user.id,
+            token: token
+        });
+        //In case of email token, create the token in the database
+
+        // try {
+        //     const createdToken = await prisma.token.create({
+        //         data: {
+        //             emailToken: token,
+        //             type: TokenType.API,
+        //             expiration: tokenExpiration,
+        //             user: {
+        //                 connect: {
+        //                     email: email
+        //                 }
+        //             }
+        //         },
+        //         select: {
+        //             emailToken: true,
+        //             user: {
+        //                 select: {
+        //                     email: true
+        //                 }
+        //             }
+        //         }
+        //     });
+        //     // //send the email token
+        //     //await sendEmailToken(email, emailToken);
+        //     res.status(201).json({
+        //         message: "login OK",
+        //         data: createdToken
+        //     });
+        // } catch (error) {
+        //     res.status(500).json({
+        //         message: "Internal Server Error!",
+        //         error: error
+        //     });
+        // }
     }
 }
 
--- src/middleware/auth.ts
+++ src/middleware/auth.ts
@@ -1,13 +1,24 @@
 import { NextFunction, Request, Response } from 'express';
-import { StatusCodes } from 'http-status-codes';
 import jwt from '../utils/jwt';
+import prisma from '../lib/prisma';
 
-const authorize = (req: Request, res: Response, next: NextFunction) => {
+const authorize = async (req: Request, res: Response, next: NextFunction) => {
 
   try {
     const token = req.headers.authorization!.split(' ')[1];
     const decode = jwt.verify(token);
     req.body.user = decode;
+    
+    // In case of email token Fetch the token from DB to verify it's valid
+    // const fetchedToken = await prisma.token.findUnique({
+    //   where: {
+    //     emailToken: token,
+    //   },
+    //   include: {
+    //     user: true,
+    //   },
+    // })
+
     next();
   } catch (error) {
     res.status(401).json({
